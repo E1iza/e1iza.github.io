d0e5baf47ac71bfbd3233ba7141eea0b8704c9c3
eliza_nov <eliza.novizkaya@gmail.com>
2024-08-28 22:05:22 +0300

    Реализован прототип десктопной версии сайта

--- scrollNavbar.js
+++ scrollNavbar.js
@@ -1,5 +1,6 @@
 window.addEventListener('scroll', () => {
   let scrollDistance = window.scrollY;
+  const windowInnerHeight = window.innerHeight
   document.querySelectorAll('.section').forEach((el, i) => {
     if (el.offsetTop - document.querySelector('nav').clientHeight - document.querySelector('header').clientHeight <= scrollDistance) {
       document.querySelectorAll('nav a').forEach((el) => {
@@ -8,6 +9,16 @@ window.addEventListener('scroll', () => {
         }
       })
       document.querySelectorAll('nav li')[i].querySelector('a').classList.add('nav-active');
+    }
+    if (document.querySelector('footer').offsetTop - windowInnerHeight <= scrollDistance) {
+      document.querySelector('.nav-contacts').classList.add('nav-active');
+        document.querySelectorAll('nav a').forEach((el) => {
+          if (el.classList.contains('nav-active')) {
+            el.classList.remove('nav-active');
+          }
+      })
+    } else {
+      document.querySelector('.nav-contacts').classList.remove('nav-active');
     }
   });
 })
